28da2ef9d3d95fc7f324420f02ddecd4f4387e1f
Aaron Arguelles <aaron.arguelles@maloufcompanies.com>
2024-04-12 14:09:24 -0600

    Game is complete and functional.
    Working on text for textboxes

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,14 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Here is my computer. I play a wide variety of video games like Helldivers 2 and Sea of Stars. Actually, my goal is to get into game development and make awesome games like those that I play!`,
+  "cs-degree": `This is my Software Engineering Certificate that I received from the bootcamp that I took from Thinkful, a Chegg Service.`,
+  "sofa-table": ``,
+  tv: ``,
+  bed: ``,
+  resume: `Here is my resume! Please feel free to check it out. You can access it by clicking <a href="/Aaron-Arguelles-Resume.pdf" target="_blank">here</a>.`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS.`,
+  library: `I'm not much of a recreational reader, but I LOVE the Empyrean series by Rebecca Yarros. There's only two books currently out, Fourth Wing and Iron Flame, but the third has been announced and I absolutely cannot wait!`,
+  exit: ``,
+};
