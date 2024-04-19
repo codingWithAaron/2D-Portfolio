9ce19d14889eef38305f43023bd4c4cb7a40baf8
Aaron Arguelles <aaron.arguelles@maloufcompanies.com>
2024-04-19 11:52:34 -0600

    added bakground music

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@ export const scaleFactor = 4;
 export const dialogueData = {
   resume: `Here is my resume! Please feel free to check it out. You can access it by clicking <a href="/Aaron-Arguelles-Resume.pdf" target="_blank">here</a>.`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. I followed a tutorial by Beau Carnes on how to do this, which was from freecodecamp.org, and made modifications to make it my own! Credit for the sprites and tilesets goes to PixiVan on itch.io and a few from the tutorial itself.`,
+  Text is rendered with HTML/CSS. I followed a tutorial by Beau Carnes and JSLegendDev on how to do this, which was from freecodecamp.org, and made modifications to make it my own! Credit for the sprites and tilesets goes to PixiVan on itch.io and a few from the tutorial itself.`,
   chest: `Contained within this chest are a couple of my favorite books. I'm not much of a recreational reader, but I LOVE the Empyrean series by Rebecca Yarros. There's only two books currently out, Fourth Wing and Iron Flame, but the third has been announced and I absolutely cannot wait!`
 };
